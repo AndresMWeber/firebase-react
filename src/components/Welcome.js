1375cb50752aebc35bae2009f75c05f71477754b
Elba Chimilio <emchimilio@gmail.com>
2020-10-05 12:21:06 -0400

    Initial profile

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,23 @@
-import React, { useContext } from "react";
-import { AuthContext } from "../App";
+import React, { useContext, Fragment } from 'react';
+import { AuthContext } from '../App';
+import '../styling/Welcome.scss';
 
 function Auth() {
   let { isLoggedIn, user } = useContext(AuthContext);
-  return <h1>Welcome {isLoggedIn && user ? user.email : "newbie"}!</h1>;
+  console.log('>>> USER', user);
+  return (
+    <Fragment>
+      <h1>Welcome {isLoggedIn && user ? user.displayName : 'newbie'}! </h1>
+      {isLoggedIn && user ? (
+        <img className='profile-pic' src={user.photoURL} />
+      ) : (
+        <img
+          className='profile-pic'
+          src='https://www.aquariumofpacific.org/images/uploads/penguin-profile.jpg'
+        />
+      )}
+    </Fragment>
+  );
 }
 
 export default Auth;
